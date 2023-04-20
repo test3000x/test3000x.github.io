a916f383d33ea224fdb87b7eac14fc665361ef37
MaHappyCode <dani.yalda@outlook.com>
2023-04-21 01:28:36 +0200

    .

--- menu.js
+++ menu.js
@@ -1,7 +1,25 @@
-const menuBtn = document.querySelector(".hamburger-menu");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
 const menu = document.querySelector(".menu");
+const shadow = document.querySelector(".shadow");
 
-menuBtn.addEventListener("click", () => {
-menu.classList.toggle("show");
-menuBtn.classList.toggle("active");
-});
+function toggleMenu() {
+  hamburgerMenu.classList.toggle("active");
+  menu.classList.toggle("show");
+  shadow.classList.toggle("show");
+
+  if (menu.classList.contains("show")) {
+    document.body.classList.add("show-shadow");
+  } else {
+    document.body.classList.remove("show-shadow");
+  }
+}
+
+function closeMenu() {
+  hamburgerMenu.classList.remove("active");
+  menu.classList.remove("show");
+  shadow.classList.remove("show");
+  document.body.classList.remove("show-shadow");
+}
+
+hamburgerMenu.addEventListener("click", toggleMenu);
+shadow.addEventListener("click", closeMenu);
